9ac89aa2f8fe9d9d0438f2fcb0ec7d5111f51fb2
asgeirsc <78665298+asgeirsc@users.noreply.github.com>
2021-03-01 14:34:09 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,7 +1,57 @@
-## You should create one R script called run_analysis.R that does the following. 
-##
-## 1. Merges the training and the test sets to create one data set.
-## 2. Extracts only the measurements on the mean and standard deviation for each measurement. 
-## 3. Uses descriptive activity names to name the activities in the data set
-## 4. Appropriately labels the data set with descriptive variable names. 
-## 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+## Read the activity labels into a data table
+activity_labels <- read.table("./UCI HAR Dataset/activity_labels.txt")
+
+## Read the features into a data tables
+features <- read.table("./UCI HAR Dataset/features.txt")[,2]
+features_meanstd <- grepl("mean|std", features)
+
+## Read the files for the 'training' data set
+## X_train: observations (561 measurements)
+## Y_train: activities (values 1-6)
+## subject_train: subjects (values 1-30)
+X_train <- read.table("./UCI HAR Dataset/train/X_train.txt") 
+Y_train <- read.table("./UCI HAR Dataset/train/Y_train.txt")
+subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt")
+
+## Add activity name to X_test
+Y_train[,2] = activities[Y_train[,1]]
+
+## Rename the columns of the data tables
+names(X_train) <- features
+names(Y_train) <- c("activity", "activityname")
+names(subject_train) <- "subject"
+
+## Bind the X_train data (only mean and std measurements) with the subject and activity name
+training <- cbind(subject_train, Y_train, X_train[,features_meanstd])
+
+## Read the files for the 'test' data set
+## X_train: observations (561 measurements)
+## Y_train: activities (values 1-6)
+## subject_train: subjects (values 1-30)
+X_test <- read.table("./UCI HAR Dataset/test/X_test.txt") 
+Y_test <- read.table("./UCI HAR Dataset/test/Y_test.txt")
+subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt")
+
+## Add activityname to Y_test
+Y_test[,2] = activities[Y_test[,1]]
+
+## Rename the columns of the data tables
+names(X_test) <- features
+names(Y_test) <- c("activity", "activityname")
+names(subject_test) <- "subject"
+
+## Bind the X_tests data (only mean and std measurements) with the subject and activity name
+test <- cbind(subject_test, Y_test, X_test[,features_meanstd])
+
+## Merge the 'training' and 'test' data sets, write result to a csv file
+merged <- rbind(training, test)
+write.csv(merged, file = "merged.csv")
+
+## Reshape the data set in order to perform calculations for each subject/activityname combination
+ids <- c("subject", "activity", "activityname")
+measures <- setdiff(names(merged),ids)
+melted <- melt(merged, id = ids, measure.vars = measures)
+
+## Calcuate the mean of each measurement, per subject/activityname
+means <- dcast(melted, subject + activityname ~ variable, mean)
+write.csv(means, file = "means.csv")
